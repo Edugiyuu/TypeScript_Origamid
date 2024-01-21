d5664c686a23135e341dac6d2cf2cdc7bf8f4967
Eduardo Santos <edupaz077@gmail.com>
2024-01-20 23:51:02 -0300

    update

---
+++ Null e Undefined/script.ts
@@ -0,0 +1,13 @@
+const button = document.querySelector('button');
+const config = localStorage.getItem('config');
+
+if (button !== null) {
+  button.click();
+}
+if (button) {
+  button.click();
+}
+if (button) button.click();
+button?.click();
+
+console.log(typeof null);
