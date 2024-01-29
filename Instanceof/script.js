22be6b320977fa8a2b00e2d7026c85df74c71b62
Eduardo Santos <edupaz077@gmail.com>
2024-01-29 00:01:11 -0300

    update

--- Instanceof/script.js
+++ Instanceof/script.js
@@ -8,6 +8,7 @@
 const livro = new Produto("Harry Potter");
 
 console.log(livro.nome); */
+// Aula - Instanceof1
 class Produto {
     nome;
     preco;
@@ -45,7 +46,50 @@ function buscarProduto(busca) {
     }
     return null;
 }
+// Aula - Instanceof1
+//Aula - InstanceOf 2
+/* class Produto{
+  nome: string;
+  constructor(nome:string){
+    this.nome = nome;
+  }
+  
+}
+
+class Jogo extends Produto{
+  jogadores: number;
+  constructor(nome:string,jogadores: number){
+    super(nome)
+    this.jogadores = jogadores;
+  }
+}
+class Livro extends Produto{
+  autor: string;
+  constructor(nome:string,autor: string){
+    //O super pelo q entendi ela é responsavel por recriar
+    //o constructor de Produto
+    super(nome)
+    this.autor = autor;
+  }
+}
+function buscarProduto(busca:string) {
+  if (busca === 'Harry Potter') {
+    return new Livro('Harry Potter','J. K. Rowling')
+  }
+  if (busca === 'Dark Souls') {
+    return new Jogo('Dark Souls',1)
+  }
+  return null
+} */
+//Aula - InstanceOf 2
 const produto = buscarProduto('Harry Potter');
+if (produto instanceof Produto) {
+    console.log(produto.nome);
+}
 if (produto instanceof Livro) {
     console.log(produto.autor);
+}
+//verifica se o produto é um livro ou um jogo (não da pra usar sem esses ifs já q pode ser Livro, Jogo ou Null)
+if (produto instanceof Jogo) {
+    console.log(produto.jogadores);
 }
