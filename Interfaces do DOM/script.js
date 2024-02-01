144789937828b945174386dff39e601f3414249e
Eduardo Santos <edupaz077@gmail.com>
2024-01-31 23:52:02 -0300

    update

--- Interfaces do DOM/script.js
+++ Interfaces do DOM/script.js
@@ -1,5 +1,27 @@
 "use strict";
-const video = document.querySelector('video');
+/* const video = document.querySelector('video');
 const video2 = document.querySelector('#videoprincipal');
-video?.volume;
-video2?.volume;
+
+if (video instanceof HTMLVideoElement){
+    console.log(video.volume);
+}
+video2?.volume; */
+const links = document.querySelectorAll('.link');
+//Não é um array
+links.forEach((link) => {
+    if (link instanceof HTMLAnchorElement) {
+        //porque não usar typeof??
+        console.log(link.href);
+    }
+});
+//Array<string>
+//NodeListOf<Element>
+//Oque é nodeList?
+//Não é um array
+function ativarElemento(elemento) {
+    elemento.style.color = 'red';
+    elemento.style.border = '2px solid red';
+}
+links.forEach((link) => {
+    ativarElemento(link);
+});
--- Interfaces do DOM/script.ts
+++ Interfaces do DOM/script.ts
@@ -1,5 +1,40 @@
-const video = document.querySelector('video');
+/* const video = document.querySelector('video');
 const video2 = document.querySelector('#videoprincipal');
 
-video?.volume
-video2?.volume
+if (video instanceof HTMLVideoElement){
+    console.log(video.volume);
+}
+video2?.volume; */
+
+const links = document.querySelectorAll('.link')
+//Não é um array
+
+
+links.forEach((link)=>{
+    if (link instanceof HTMLAnchorElement) {
+        //porque não usar typeof??
+        console.log(link.href);
+    }
+})
+//Array<string>
+//NodeListOf<Element>
+//Oque é nodeList?
+
+//Não é um array
+
+function ativarElementoAnchor(elemento: HTMLAnchorElement) {
+    elemento.style.color = 'red';
+    elemento.style.border = '2px solid red'
+}
+function ativarElementoButton(elemento: HTMLButtonElement) {
+    elemento.style.color = 'red';
+    elemento.style.border = '2px solid red'
+}
+links.forEach((link)=>{
+   if (link instanceof HTMLAnchorElement) {
+    ativarElementoAnchor(link)
+   }
+   if (link instanceof HTMLButtonElement) {
+    ativarElementoButton(link);
+   }
+})
